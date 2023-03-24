d24bbf8b3bb1f6defe2f4b37d0315d393c13d85b
lfchaves18 <lfcc18@gmail.com>
2023-03-24 18:20:45 -0300

    feat: adicionando construtor a classe

--- Calcular.java
+++ Calcular.java
@@ -1,17 +1,22 @@
 public class Calcular {
 
+    int totalVotos;
+    public Calcular(int totalVotos) {
+        this.totalVotos = totalVotos;
+    }
+
     //Metódo que calcula os votos válidos
-    public double calculaPercentualVotosValidos(int totalVotos, int votosValidos){
+    public double calculaPercentualVotosValidos(int votosValidos){
         return ((double)votosValidos / totalVotos) * 100.0;
     }
 
     //Metódo que calcula os votos brancos
-    public double calculaPercentualVotosBrancos(int totalVotos,  int votosBrancos){
+    public double calculaPercentualVotosBrancos(int votosBrancos){
         return ((double)votosBrancos / totalVotos) * 100.0;
     }
 
     //Metódo que calcula os votos nulos
-    public double calculaPercentualVotosNulos(int totalVotos, int votosNulos){
+    public double calculaPercentualVotosNulos(int votosNulos){
         return ((double)votosNulos / totalVotos) * 100.0;
     }
 }
--- Main.java
+++ Main.java
@@ -2,11 +2,11 @@ public class Main {
 
     public static void main(String [] args){
         //Instanciando a classe calcular que possui os metódos
-        Calcular calcular = new Calcular();
+        Calcular calcular = new Calcular(1000);
 
         //Fazendo as chamadas das funções e mostrando na tela os resultados
-        System.out.println("O percentual de votos válidos é: " + calcular.calculaPercentualVotosValidos(1000,800) +"%");
-        System.out.println("O percentual de votos brancos é: " + calcular.calculaPercentualVotosBrancos(1000,150) +"%");
-        System.out.println("O percentual de votos nulos é: " + calcular.calculaPercentualVotosNulos(1000,50) +"%");
+        System.out.println("O percentual de votos válidos é: " + calcular.calculaPercentualVotosValidos(800) +"%");
+        System.out.println("O percentual de votos brancos é: " + calcular.calculaPercentualVotosBrancos(150) +"%");
+        System.out.println("O percentual de votos nulos é: " + calcular.calculaPercentualVotosNulos(50) +"%");
     }
 }
